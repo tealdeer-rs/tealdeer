152343eaeff4a363aede2e9b987be60d7c2b96cd
Danilo Bargen <mail@dbrgn.ch>
2016-01-26 10:22:03 +0100

    Underline parameters in example code

--- src/formatter.rs
+++ src/formatter.rs
@@ -2,10 +2,26 @@
 
 use std::io::BufRead;
 
-use ansi_term::Colour;
+use ansi_term::{Colour, ANSIStrings};
 
 use tokenizer::Tokenizer;
 use types::LineType;
+
+
+/// Provide formatting for {{ curly braces }} in ExampleCode lines
+fn format_braces(text: &str) -> String {
+    let parts = text.split("{{").flat_map(|s| s.split("}}"))
+                    .enumerate()
+                    .map(|(i, v)| {
+                        if i % 2 == 0 {
+                            Colour::Cyan.paint(v)
+                        } else {
+                            Colour::Cyan.underline().paint(v)
+                        }
+                    })
+                    .collect::<Vec<_>>();
+    return ANSIStrings(&parts).to_string()
+}
 
 /// Print a token stream to an ANSI terminal.
 pub fn print_lines<R>(tokenizer: &mut Tokenizer<R>) where R: BufRead {
@@ -15,7 +31,7 @@ pub fn print_lines<R>(tokenizer: &mut Tokenizer<R>) where R: BufRead {
             LineType::Title(_) => debug!("Ignoring title"),
             LineType::Description(text) => println!("  {}", text),
             LineType::ExampleText(text) => println!("  {}", Colour::Green.paint(format!("- {}", text))),
-            LineType::ExampleCode(text) => println!("  {}", Colour::Cyan.paint(format!("  {}", text))),
+            LineType::ExampleCode(text) => println!("  {}", &format_braces(&text)),
             LineType::Other(text) => debug!("Unknown line type: {:?}", text),
         }
     }
