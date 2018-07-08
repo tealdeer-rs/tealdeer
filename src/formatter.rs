1bf8c1b62f4cd565936de78de5582fd5965ed4ed
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2018-07-08 17:37:21 +0200

    Make colour scheme high contrast
    Highlight program name in code examples

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,36 +1,58 @@
 //! Functions related to formatting and printing lines from a `Tokenizer`.
 
+use std::fmt::Write;
 use std::io::BufRead;
 
-use ansi_term::{Colour, ANSIStrings};
+use ansi_term::{Colour, Style};
 
 use tokenizer::Tokenizer;
 use types::LineType;
 
 /// Provide formatting for {{ curly braces }} in ExampleCode lines
-fn format_braces(text: &str) -> String {
-    let parts = text.split("{{").flat_map(|s| s.split("}}"))
-                    .enumerate()
-                    .map(|(i, v)| {
-                        if i % 2 == 0 {
-                            Colour::Cyan.paint(v)
-                        } else {
-                            Colour::Cyan.underline().paint(v)
-                        }
-                    })
-                    .collect::<Vec<_>>();
-    ANSIStrings(&parts).to_string()
+fn format_code(command: &str, text: &str) -> String {
+    let mut parts = String::new();
+    parts.reserve(text.len() * 3);
+
+    for part in text.split("}}") {
+        let var_begin_op = part.find("{{");
+        let command_begin_op = part[..var_begin_op.unwrap_or(part.len())].find(&command);
+
+        let begin = command_begin_op.map(|v| v + command.len()).unwrap_or(0);
+        let end = var_begin_op.unwrap_or(part.len());
+
+        if let Some(command_begin) =  command_begin_op {
+            parts.push_str(&part[..command_begin]);
+
+            write!(parts, "{}", Colour::Red.paint(command)).unwrap();
+        }
+
+        parts.push_str(&part[begin..end]);
+
+        if let Some(var_begin) = var_begin_op {
+            let var_slice = &part[var_begin + 2..];
+            write!(parts, "{}", Style::default().underline().paint(var_slice)).unwrap();
+        }
+    }
+
+    parts
 }
 
 /// Print a token stream to an ANSI terminal.
 pub fn print_lines<R>(tokenizer: &mut Tokenizer<R>) where R: BufRead {
+    let mut command = String::new();
     while let Some(token) = tokenizer.next_token() {
         match token {
             LineType::Empty => println!(""),
-            LineType::Title(_) => debug!("Ignoring title"),
-            LineType::Description(text) => println!("  {}", text),
-            LineType::ExampleText(text) => println!("  {}", Colour::Green.paint(text)),
-            LineType::ExampleCode(text) => println!("      {}", &format_braces(&text)),
+            LineType::Title(title) => {
+                debug!("Ignoring title");
+
+                // This is safe as long as the parsed title is only the command,
+                // and tokenizer yields values in order of appearance.
+                command = title;
+            },
+            LineType::Description(text) => println!("  {}", Colour::White.paint(text)),
+            LineType::ExampleText(text) => println!("  {}", text),
+            LineType::ExampleCode(text) => println!("      {}", &format_code(&command, &text)),
             LineType::Other(text) => debug!("Unknown line type: {:?}", text),
         }
     }
