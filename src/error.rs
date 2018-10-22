6fcb1e17de6ad640ef0ff5a9bcc501e4d6f7e8da
Danilo Bargen <mail@dbrgn.ch>
2018-10-22 18:03:07 +0200

    Impl Display for TealdeerError

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use curl::Error as CurlError;
 
 #[derive(Debug)]
@@ -10,5 +11,15 @@ pub enum TealdeerError {
 impl From<CurlError> for TealdeerError {
     fn from(err: CurlError) -> TealdeerError {
         TealdeerError::UpdateError(format!("Curl error: {}", err.to_string()))
+    }
+}
+
+impl fmt::Display for TealdeerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TealdeerError::CacheError(e) => write!(f, "CacheError: {}", e),
+            TealdeerError::ConfigError(e) => write!(f, "ConfigError: {}", e),
+            TealdeerError::UpdateError(e) => write!(f, "UpdateError: {}", e),
+        }
     }
 }
