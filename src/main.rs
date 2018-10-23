5369f1493fa9428b0b09a36590049543115ecb0a
shaanmuhammad <42970482+shaanmuhammad@users.noreply.github.com>
2018-10-23 13:36:48 +0500

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. All files in the project carrying such notice may not be
 // copied, modified, or distributed except according to those terms.
+//classes are declared
 
 #![deny(
     missing_docs,
