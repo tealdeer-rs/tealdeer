7f8cbb01e5b133ededf381fde12ed914069bd226
Korrat <korrat@protonmail.com>
2019-10-03 11:55:07 +0200

    Suppress empty lines for compact output

--- src/formatter.rs
+++ src/formatter.rs
@@ -57,7 +57,11 @@ where
     let mut command = String::new();
     while let Some(token) = tokenizer.next_token() {
         match token {
-            LineType::Empty => println!(),
+            LineType::Empty => {
+                if !config.display.compact {
+                    println!()
+                }
+            }
             LineType::Title(title) => {
                 debug!("Ignoring title");
 
