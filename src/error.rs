a594b0013e63d9996db161379492f0ec3e4e24a0
Danilo Bargen <mail@dbrgn.ch>
2020-02-28 01:11:10 +0100

    Fix clippy warnings (#105)

--- src/error.rs
+++ src/error.rs
@@ -11,16 +11,16 @@ pub enum TealdeerError {
 
 impl From<ReqwestError> for TealdeerError {
     fn from(err: ReqwestError) -> Self {
-        TealdeerError::UpdateError(format!("HTTP error: {}", err.to_string()))
+        Self::UpdateError(format!("HTTP error: {}", err.to_string()))
     }
 }
 
 impl fmt::Display for TealdeerError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            TealdeerError::CacheError(e) => write!(f, "CacheError: {}", e),
-            TealdeerError::ConfigError(e) => write!(f, "ConfigError: {}", e),
-            TealdeerError::UpdateError(e) => write!(f, "UpdateError: {}", e),
+            Self::CacheError(e) => write!(f, "CacheError: {}", e),
+            Self::ConfigError(e) => write!(f, "ConfigError: {}", e),
+            Self::UpdateError(e) => write!(f, "UpdateError: {}", e),
         }
     }
 }
