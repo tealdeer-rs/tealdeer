95eb8ef4d6e56e5afd8b802e9bfb26920de70bff
Tomás Farías Santana <tomas@tomasfarias.dev>
2020-10-31 21:34:07 +0100

    Add support for ANSI code and RGB colors

--- src/config.rs
+++ src/config.rs
@@ -34,6 +34,8 @@ pub enum RawColor {
     Purple,
     Cyan,
     White,
+    ANSI(u8),
+    RGB{r: u8, g: u8, b: u8},
 }
 
 impl From<RawColor> for Color {
@@ -47,6 +49,8 @@ impl From<RawColor> for Color {
             RawColor::Purple => Self::Purple,
             RawColor::Cyan => Self::Cyan,
             RawColor::White => Self::White,
+            RawColor::ANSI(num) => Self::Fixed(num),
+            RawColor::RGB {r, g, b} => Self::RGB(r, g, b),
         }
     }
 }
