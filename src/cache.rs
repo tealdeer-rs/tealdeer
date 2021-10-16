deadf74251747eb9302121a63b6db5311125d9ff
invakid404 <invakid404@riseup.net>
2021-10-16 19:51:47 +0300

    chore: wipe only pages instead of entire cache dir

--- src/cache.rs
+++ src/cache.rs
@@ -334,12 +334,18 @@ impl Cache {
     pub fn clear() -> Result<(), TealdeerError> {
         let (path, _) = Self::get_cache_dir()?;
         if path.exists() && path.is_dir() {
-            fs::remove_dir_all(&path).map_err(|_| {
-                CacheError(format!(
-                    "Could not remove cache directory ({}).",
-                    path.display()
-                ))
-            })?;
+            // Delete old tldr-pages cache location as well if present
+            // TODO: To be removed in the future
+            for page_dir in [TLDR_PAGES_DIR, TLDR_OLD_PAGES_DIR] {
+                let pages_dir = path.join(page_dir);
+
+                fs::remove_dir_all(&pages_dir).map_err(|_| {
+                    CacheError(format!(
+                        "Could not remove cache directory ({}).",
+                        pages_dir.display()
+                    ))
+                })?;
+            }
         } else if path.exists() {
             return Err(CacheError(format!(
                 "Cache path ({}) is not a directory.",
