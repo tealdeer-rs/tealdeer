ea0a40d56812419a61183a57cea867cce938f84c
Pavel Timofeev <pav.timofeev@gmail.com>
2026-03-11 15:49:26 -0300

    improve help output by adding examples section

--- src/cli.rs
+++ src/cli.rs
@@ -6,6 +6,21 @@ use clap::{builder::ArgAction, ArgGroup, Parser};
 
 use crate::types::{ColorOptions, PlatformType};
 
+const AFTER_HELP: &str = r#"To view the user documentation, please visit https://tealdeer-rs.github.io/tealdeer/.
+
+EXAMPLES:
+# View the TLDR page for ip:
+tldr ip
+
+# View a multi-word TLDR page:
+tldr git rebase
+
+# Update TLDR with pages for a specific language:
+tldr --update --language es
+
+# Specify the language for the page:
+tldr tar --language es"#;
+
 // Note: flag names are specified explicitly in clap attributes
 // to improve readability and allow contributors to grep names like "clear-cache"
 #[derive(Parser, Debug)]
@@ -18,7 +33,7 @@ use crate::types::{ColorOptions, PlatformType};
 {usage-heading} {usage}
 
 {all-args}{after-help}",
-    after_help = "To view the user documentation, please visit https://tealdeer-rs.github.io/tealdeer/.",
+    after_help = AFTER_HELP,
     arg_required_else_help = true,
     help_expected = true,
     group = ArgGroup::new("command_or_file").args(&["command", "render"]),
